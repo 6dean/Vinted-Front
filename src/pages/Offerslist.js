790652ecb22443a44c2914b109f7430dd8d4865b
6dean <ferdjallahrockdean@gmail.com>
2022-11-12 16:55:04 +0100

    undry method

--- src/pages/Offerslist.js
+++ src/pages/Offerslist.js
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 const Offerslist = () => {
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [sortasc, setSortasc] = useState(false);
+  const [sortdesc, setSortdesc] = useState(false);
 
   const fetchData = async () => {
     const response = await axios.get(
       "https://site--backend-vinted--6qn7tv96v7tt.code.run/offers"
     );
     setData(response.data);
-
     setIsLoading(false);
   };
 
@@ -21,53 +22,159 @@ const Offerslist = () => {
 
   return isLoading ? (
     <div className="loading">
-      <p className="loading-text">LOADING ...</p>
+      <div className="loading-text">LOADING ...</div>
     </div>
   ) : (
     <>
+      <div className="nav-bar-offers">
+        <button
+          onClick={() => {
+            setSortasc(true);
+            setSortdesc(false);
+          }}
+        >
+          Moins cher en premier
+        </button>
+        <button
+          onClick={() => {
+            setSortdesc(true);
+            setSortasc(false);
+          }}
+        >
+          Plus cher en premier
+        </button>
+      </div>
       <div className="introduction">
-        {data.map((elem, index) => {
-          return (
-            <div key={index}>
-              {elem.owner !== undefined ? (
-                <div className="sell-card">
-                  <div className="user-id">
-                    <img
-                      className="avatar"
-                      src={elem.owner.avatar.secure_url}
-                      alt=""
-                    />
-
-                    {elem.owner === undefined
-                      ? "Team Reacteur"
-                      : elem.owner.account.username}
-                  </div>
-
-                  <Link to={`/offer/${elem._id}`}>
-                    <div className="image-container">
+        {!sortasc &&
+          !sortdesc &&
+          data.map((elem, index) => {
+            return (
+              <div key={index}>
+                {elem.owner !== undefined ? (
+                  <div className="sell-card">
+                    <div className="user-id">
                       <img
-                        src={
-                          elem.product_image.secure_url
-                            ? elem.product_image.secure_url
-                            : null
-                        }
+                        className="avatar"
+                        src={elem.owner.avatar.secure_url}
                         alt=""
                       />
                     </div>
-                  </Link>
 
-                  <div className="product-price">{elem.product_price} €</div>
-                  <div className="product-info">
-                    {elem.product_details[1].size}
+                    <Link to={`/offer/${elem._id}`}>
+                      <div className="image-container">
+                        <img
+                          src={
+                            elem.product_image.secure_url
+                              ? elem.product_image.secure_url
+                              : null
+                          }
+                          alt=""
+                        />
+                      </div>
+                    </Link>
+
+                    <div className="product-price">{elem.product_price} €</div>
+                    <div className="product-info">
+                      {elem.product_details[1].size}
+                    </div>
+                    <div className="product-info">
+                      {elem.product_details[0].brand}
+                    </div>
                   </div>
-                  <div className="product-info">
-                    {elem.product_details[0].brand}
-                  </div>
+                ) : null}
+              </div>
+            );
+          })}
+        {sortasc &&
+          data
+            .sort((a, b) => {
+              return a.product_price - b.product_price;
+            })
+            .map((elem, index) => {
+              return (
+                <div key={index}>
+                  {elem.owner !== undefined ? (
+                    <div className="sell-card">
+                      <div className="user-id">
+                        <img
+                          className="avatar"
+                          src={elem.owner.avatar.secure_url}
+                          alt=""
+                        />
+                      </div>
+
+                      <Link to={`/offer/${elem._id}`}>
+                        <div className="image-container">
+                          <img
+                            src={
+                              elem.product_image.secure_url
+                                ? elem.product_image.secure_url
+                                : null
+                            }
+                            alt=""
+                          />
+                        </div>
+                      </Link>
+
+                      <div className="product-price">
+                        {elem.product_price} €
+                      </div>
+                      <div className="product-info">
+                        {elem.product_details[1].size}
+                      </div>
+                      <div className="product-info">
+                        {elem.product_details[0].brand}
+                      </div>
+                    </div>
+                  ) : null}
                 </div>
-              ) : null}
-            </div>
-          );
-        })}
+              );
+            })}
+        {sortdesc &&
+          data
+            .sort((a, b) => {
+              return b.product_price - a.product_price;
+            })
+            .map((elem, index) => {
+              return (
+                <div key={index}>
+                  {elem.owner !== undefined ? (
+                    <div className="sell-card">
+                      <div className="user-id">
+                        <img
+                          className="avatar"
+                          src={elem.owner.avatar.secure_url}
+                          alt=""
+                        />
+                      </div>
+
+                      <Link to={`/offer/${elem._id}`}>
+                        <div className="image-container">
+                          <img
+                            src={
+                              elem.product_image.secure_url
+                                ? elem.product_image.secure_url
+                                : null
+                            }
+                            alt=""
+                          />
+                        </div>
+                      </Link>
+
+                      <div className="product-price">
+                        {elem.product_price} €
+                      </div>
+                      <div className="product-info">
+                        {elem.product_details[1].size}
+                      </div>
+                      <div className="product-info">
+                        {elem.product_details[0].brand}
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              );
+            })}
       </div>
     </>
   );
